f1d5a31a5d70227c3f5e6294352d546b9a33ae97
Victor Borzaquel <victorborzaquel@outlook.com>
2023-03-09 18:51:57 -0300

    Initial commit

---
+++ src/main/java/com/api/loginbancario/LoginBancarioApplication.java
@@ -0,0 +1,13 @@
+package com.api.loginbancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginBancarioApplication.class, args);
+	}
+
+}
