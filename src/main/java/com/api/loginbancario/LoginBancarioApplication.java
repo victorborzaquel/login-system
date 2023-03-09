8fa7d3d9dffcf920343f140126991999404b8f5e
Victor Borzaquel <victorborzaquel@outlook.com>
2023-03-09 19:09:32 -0300

    chore: add jacoco

--- src/main/java/com/api/loginbancario/LoginBancarioApplication.java
+++ src/main/java/com/api/loginbancario/LoginBancarioApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginBancarioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoginBancarioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoginBancarioApplication.class, args);
+    }
 
 }
--- src/test/java/com/api/loginbancario/LoginBancarioApplicationTests.java
+++ src/test/java/com/api/loginbancario/LoginBancarioApplicationTests.java
@@ -3,11 +3,14 @@ package com.api.loginbancario;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class LoginBancarioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertEquals(1, 1);
+    }
 
 }
