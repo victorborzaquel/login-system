0611ba551de792141f3a04518a1b597f195f2a04
Victor Borzaquel <victorborzaquel@outlook.com>
2023-03-10 01:56:27 -0300

    refactor: project struture

---
+++ src/main/java/com/vb/loginbancario/security/auth/AuthRepository.java
@@ -0,0 +1,13 @@
+package com.vb.loginbancario.security.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface AuthRepository extends JpaRepository<Auth, Integer> {
+    Optional<Auth> findByAccountNumber(String username);
+}
