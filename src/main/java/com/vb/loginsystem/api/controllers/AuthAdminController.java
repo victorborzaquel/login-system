8a622795359509f5d9dc4baff2f10a78f8885f1e
Victor Borzaquel <victorborzaquel@outlook.com>
2023-03-17 16:53:20 -0300

    docs: hidden constrollers

--- src/main/java/com/vb/loginsystem/api/controllers/AuthAdminController.java
+++ src/main/java/com/vb/loginsystem/api/controllers/AuthAdminController.java
@@ -1,9 +1,14 @@
 package com.vb.loginsystem.api.controllers;
 
 import com.vb.loginsystem.security.auth.AuthService;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin")
--- src/main/java/com/vb/loginsystem/api/controllers/OauthController.java
+++ src/main/java/com/vb/loginsystem/api/controllers/OauthController.java
@@ -1,9 +1,13 @@
 package com.vb.loginsystem.api.controllers;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/oauth")
